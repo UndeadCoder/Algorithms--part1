586a9bcbc16267370e9ecb3683a2ffe7d91a831a
UndeadCoder <33629085+UndeadCoder@users.noreply.github.com>
2017-12-11 19:49:20 +0800

    a small example

---
+++ examples/src/primarysorts/Selection.java
@@ -0,0 +1,54 @@
+package primarysorts;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Selection {
+  public static void sort(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      int min = i;
+      for (int j = i + 1; j < a.length; j++) {
+        if (less(a[j], a[min])) {
+          min = j;
+        }
+      }
+      exch(a, i, min);
+    }
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
+
+  private static void exch(Comparable[] a, int i, int j) {
+    Comparable t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+  }
+
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      StdOut.print(a[i] + " ");
+    }
+    StdOut.println();
+  }
+
+  public static boolean isSorted(Comparable[] a) {
+    for (int i = 1; i < a.length; i++) {
+      if (less(a[i], a[i - 1])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Double[] randomArray = new Double[1000];
+    for (int i = 0; i < randomArray.length; i++ ) {
+      randomArray[i] = StdRandom.uniform();
+    }
+    sort(randomArray);
+    show(randomArray);
+  }
+}
+
